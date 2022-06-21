c614e0018eca53311a91e3f778dc20b576e893d6
Your Name <you@example.com>
2022-06-21 12:48:24 +0100

    updated with some new app messages

---
+++ AnuOluwapoproj/AnuOluwapoproj/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for AnuOluwapoproj project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'AnuOluwapoproj.settings')
+
+application = get_asgi_application()
